8f7bbf0e568b53481f2d0736417289b0675414a4
Diegoa577 <diego.andres420@gmail.com>
2018-06-12 22:10:14 -0500

    Final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 //para hacer promesas
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//development and local
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 //server
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyparser = require('body-parser');
@@ -8,7 +10,7 @@ var {Todo} = require("./models/todo");
 var {user} = require("./models/user");
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyparser.json());
 
